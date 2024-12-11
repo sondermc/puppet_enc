d2a6499245d3b3083a247c28fb3ae855d7dad06a
Chris Sondermeijer <chris.sondermeijer@gmail.com>
2024-12-11 11:03:56 +0100

    refactored

--- src/db.rs
+++ src/db.rs
@@ -1,9 +1,27 @@
 use std::result::Result;
-use sqlx::{sqlite::SqliteQueryResult, SqlitePool};
+use sqlx::{sqlite::SqliteQueryResult, Error, SqlitePool };
 
-pub async fn create_schema(db_url:&str) -> Result<SqliteQueryResult, sqlx::Error>{
+pub async fn create_schema(db_url:&str) -> Result<SqliteQueryResult, Error>{
     let pool = SqlitePool::connect(&db_url).await?;
     let result = sqlx::query_file!("db/schema.sql").execute(&pool).await;
     pool.close().await;
     return result;
 }
+
+pub async fn insert_data(db_url:&str) -> Result<SqliteQueryResult, Error>{
+    let pool = SqlitePool::connect(&db_url).await?;
+    let result = sqlx::query_file!("db/data.sql").execute(&pool).await;
+    pool.close().await;
+    return result;
+}
+
+
+/*
+  let instances = SqlitePool::connect(&db_url).await.unwrap();
+  let qry = "INSERT INTO environment (name, description) VALUES($1, $2)";
+  let insert_query = sqlx::query(&qry).bind("test").bind("The example.com Testing Environment").execute(&instances).await;
+  
+  instances.close().await;
+  println!("{:?}", insert_query);
+
+  */
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod db;
 use puppet_enc::set_nodename;
 
 use std::env;
-use sqlx::{Sqlite, SqlitePool, migrate::MigrateDatabase};
+use sqlx::migrate::MigrateDatabase;
 
 #[async_std::main]
 async fn main(){
@@ -11,18 +11,16 @@ async fn main(){
   println!("final nodename: {}", nodename);
   
   let db_url = String::from("sqlite://db/puppet_enc.sqlite"); 
-  if !Sqlite::database_exists(&db_url).await.unwrap_or(false){
-    Sqlite::create_database(&db_url).await.unwrap();
+  
+  if !sqlx::Sqlite::database_exists(&db_url).await.unwrap_or(false) {
+    sqlx::Sqlite::create_database(&db_url).await.unwrap();
     match db::create_schema(&db_url).await{
       Ok(_) => println!("Database created succesfully"),
       Err(e) => panic!("{}", e)
-      
+    }
+    match db::insert_data(&db_url).await{
+      Ok(_) => println!("Data added to database succesfully"),
+      Err(e) => panic!("{}", e)
     }
   }
-  let instances = SqlitePool::connect(&db_url).await.unwrap();
-  let qry = "INSERT INTO environment (name, description) VALUES($1, $2)";
-  let insert_query = sqlx::query(&qry).bind("test").bind("The example.com Testing Environment").execute(&instances).await;
-  
-  instances.close().await;
-  println!("{:?}", insert_query);
 }
