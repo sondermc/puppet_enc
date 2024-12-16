18948099831f83dbd86ac6aa6d1ba32f81603283
Chris Sondermeijer <chris.sondermeijer@gmail.com>
2024-12-16 14:18:32 +0100

    dearing database_url from .env file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 mod db;
 use puppet_enc::set_nodename;
-
 use std::env;
-
 use sqlx::{migrate::MigrateDatabase, Sqlite};
-
 use log::debug;
-
-const DB_URL: &str = "sqlite://db/puppet_enc.sqlite?mode=rwc";
+use dotenv::dotenv;
 
 #[async_std::main]
 async fn main(){
@@ -16,14 +12,18 @@ async fn main(){
   let args: Vec<String> = env::args().collect();
   let nodename: String = set_nodename(args);
   debug!("final nodename: {}", nodename);
-  
-  if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
-    Sqlite::create_database(DB_URL).await.unwrap();
-    match db::create_schema(DB_URL).await{
+
+  dotenv().ok();
+  let database_url: String = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+  let db_url = database_url.as_str();
+
+  if !Sqlite::database_exists(db_url).await.unwrap_or(false) {
+    Sqlite::create_database(db_url).await.unwrap();
+    match db::create_schema(db_url).await{
       Ok(_) => debug!("Database created succesfully"),
       Err(e) => panic!("{}", e)
     }
-    match db::insert_data(DB_URL).await{
+    match db::insert_data(db_url).await{
       Ok(_) => debug!("Data added to database succesfully"),
       Err(e) => panic!("{}", e)
     }
